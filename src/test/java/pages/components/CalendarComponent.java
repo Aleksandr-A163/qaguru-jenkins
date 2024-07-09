910e53ce4f51be0047b61343fd217d2d8fb5343d
Aleksandr Anosov <you@example.com>
2024-07-09 16:39:33 +0300

    added tests and Allure report

--- src/test/java/pages/components/CalendarComponent.java
+++ src/test/java/pages/components/CalendarComponent.java
@@ -1,15 +1,15 @@
 package pages.components;
 
-import static com.codeborne.selenide.Condition.text;
+
 import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.$$;
+
 
 public class CalendarComponent {
 
     public void setDate(String day, String month, String year) {
         $(".react-datepicker__month-select").selectOption(month);
         $(".react-datepicker__year-select").selectOption(year);
-        $$(".react-datepicker__day:not(.react-datepicker__day--outside-month)")
-                .filterBy(text(day)).first().click();
+            String formattedDay =  Integer.parseInt(day) > 9 ? day : "0" + day;
+            $(String.format(".react-datepicker__day--0%s:not(.react-datepicker__day--outside-month)", formattedDay)).click();
     }
 }
